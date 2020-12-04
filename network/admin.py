8803d3db1b7f7447a7a54651ee4e68085df8d0eb
Raul Rivera <raul.jose.rivera777@gmail.com>
2020-12-04 13:41:42 -0500

    add admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import User, Pagepost, Infollow, Plike
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Pagepost)
+admin.site.register(Infollow)
+admin.site.register(Plike)
